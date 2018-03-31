d13f663cb823537d36fbd5f0367ba697b374130a
Jarppe <jarppe@metosin.fi>
2018-03-31 16:05:01 +0300

    Add README and tune code a bit

---
+++ README.md
@@ -0,0 +1,19 @@
+# About this
+
+This is just a slide show for my parents on their 50th anniversary.
+
+# License
+
+## Artistic content
+
+All images Copyright <C2><A9> 1999 - 2018 Titta, Milla, Kaapo, Kristiina, Maria, Sasu and Jarppe Länsiö.
+
+## Images
+
+The images are licensed under a [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-nc-sa/4.0/).
+
+## Code
+
+All other parts Copyright (c) 2018 Jarppe Länsiö
+
+Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 "use strict";
 
-var page = 1,
-    max = 7;
-
+var page = 1
+const max = 7
 
 const next = (_) => {
-  console.log("next: page", page);
-  page++;
-  if (page > max) page = 1;
-  let img = document.getElementById("slide-img");
-  img.src = "img/Slide" + page + ".jpg";
+  console.log("next: page", page)
+  page++
+  if (page > max) page = 1
+  let img = document.getElementById("slide-img")
+  img.src = "img/Slide" + page + ".jpg"
 }
 
-document.getElementById("slide").addEventListener("click", next);
+document
+  .getElementById("slide")
+  .addEventListener("click", next)
+
+console.log("This is just a slide show for my parents on their 50th anniversary.")
+console.log("See https://github.com/jarppe/sirkka-ja-matti-50v-yhdessa")
