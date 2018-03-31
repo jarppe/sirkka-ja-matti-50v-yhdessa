c1caba6dc53c69110188b56ff99a79ed478c00ca
Jarppe <jarppe@metosin.fi>
2018-03-31 16:11:50 +0300

    Simplify, center image, etc...

--- app.js
+++ app.js
@@ -2,18 +2,16 @@
 
 var page = 1
 const max = 7
+const img = document.getElementById("slide-img")
 
 const next = (_) => {
   console.log("next: page", page)
   page++
   if (page > max) page = 1
-  let img = document.getElementById("slide-img")
   img.src = "img/Slide" + page + ".jpg"
 }
 
-document
-  .getElementById("slide")
-  .addEventListener("click", next)
+document.body.addEventListener("click", next)
 
 console.log("This is just a slide show for my parents on their 50th anniversary.")
 console.log("See https://github.com/jarppe/sirkka-ja-matti-50v-yhdessa")
