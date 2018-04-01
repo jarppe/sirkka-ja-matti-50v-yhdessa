34931b1071fd70939e5a6e1ffab6fd38958f8fc3
Jarppe <jarppe@metosin.fi>
2018-04-01 17:20:57 +0300

    better navigarion

--- app.js
+++ app.js
@@ -1,20 +1,58 @@
 "use strict";
 
+console.log("This is just a slide show for my parents on their 50th anniversary.")
+console.log("See https://github.com/jarppe/sirkka-ja-matti-50v-yhdessa")
+
+const byId = id => document.getElementById(id)
+
 var page = 1
 const max = 12
-const img = document.getElementById("slide-img")
 
 const pad = n => n.toString().padStart(2, "0")
 
-const next = (_) => {
-  console.log("next: page", page)
-  page++
-  if (page > max) page = 1
-  console.log("src", "img/Slide" + pad(page) + ".jpg")
-  img.src = "img/Slide" + pad(page) + ".jpg"
+const next = direction => {
+  return () => {
+    page += direction;
+    if (page <= 0) page = max
+    if (page > max) page = 1
+    byId("slide-img").src = "img/Slide" + pad(page) + ".jpg"
+  }
 }
 
-document.body.addEventListener("click", next)
+const goForward = next(+1)
+const goBack = next(-1)
 
-console.log("This is just a slide show for my parents on their 50th anniversary.")
-console.log("See https://github.com/jarppe/sirkka-ja-matti-50v-yhdessa")
+byId("forward").addEventListener("click", goForward)
+byId("back").addEventListener("click", goBack)
+
+const keyCodes = {
+  "ArrowLeft": goBack,
+  "ArrowRight": goForward,
+  "Space": goForward,
+  "Enter": goForward
+}
+
+document.addEventListener("keydown", e => {
+  let handler = keyCodes[e.code]
+  if (handler) {
+    e.preventDefault()
+    handler()
+  }
+})
+
+var timeoutHandle = null
+
+const showGuides = () => byId("guides").className = "show"
+const hideGuides = () => byId("guides").className = "hide"
+
+const mousemove = _ => {
+  if (timeoutHandle) {
+    clearTimeout(timeoutHandle)
+  } else {
+    showGuides()
+  }
+  timeoutHandle = setTimeout(() => { timeoutHandle = null; hideGuides(); }, 1000)
+}
+
+hideGuides()
+document.body.addEventListener("mousemove", mousemove)
